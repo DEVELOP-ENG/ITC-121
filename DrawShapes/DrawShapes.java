8fa3e15edec1d102a2e576c3c3f08fbb39e5f27d
DEVELOP-ENG <60213834+DEVELOP-ENG@users.noreply.github.com>
2020-07-06 15:09:13 -0700

    1
    1

---
+++ DrawShapes/DrawShapes.java
@@ -0,0 +1,17 @@
+package DrawShapes;
+import java.sql.Struct;
+import java.util.Scanner;
+
+public class DrawShapes {
+    
+    public static void main(String[] args) {
+
+        String topSq = new String("+------+");
+        String leftSq = new String("|      |");
+        
+        System.out.println(topSq);
+        System.out.println(leftSq);
+        System.out.println(leftSq);
+        System.out.println(topSq);
+    }
+}
