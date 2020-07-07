825f809a7653fbd63113ba0ec94da604b0c5af40
DEVELOP-ENG <60213834+DEVELOP-ENG@users.noreply.github.com>
2020-07-07 16:45:45 -0700

    new java

--- Homework/Wk1.java
+++ Homework/Wk1.java
@@ -1,24 +1,36 @@
 package Homework;
 import java.util.Scanner;
+import java.text.DecimalFormat;
 public class Wk1 {
 
 public static void main(String[] args) {
-    /*int thisInteger = 5;
-    boolean thisTrue = false; 
-    double thisDouble = 5.14123456789;*/
+
+    // tool to format numbers to certain decimal place
+    DecimalFormat format = new DecimalFormat("##.00");
+
+    // variables used to input data and for equations 
     Scanner myScan = new Scanner(System.in);
     double price; 
     double tax = 1.1; 
+
+    // attempt to create a for loop, but never finished
     boolean dinner = true;
     int q = 0;
 
-    price = myScan.nextInt();
+    // display message for initial question and interactive
+    System.out.println("How much was your meal?");
+
+    // input variable, and equations for corresponding costs
+    price = myScan.nextDouble();
     double tip = (price * tax) * 0.2; 
-do{
-   System.out.println((price * tax) + tip );
-    q++;
-        }while (q < 10);
-        
-        
+    double cost = ((price * tax) + tip);
+    double taxTotal = (price * 0.1);
+  
+// A series of displays for the varrying costs of the meal
+   System.out.println("Your subtotal of the meal is $" + format.format(price));
+   System.out.println("Your total tax of the meal is $" + format.format(taxTotal));
+   System.out.println("Your total tip of the meal is $" + format.format(tip));
+   System.out.println("Your total cost of the meal is $" + format.format(cost));   
+
     } 
 }
